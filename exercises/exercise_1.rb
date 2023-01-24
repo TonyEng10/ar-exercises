7fd20e7c8724757719f9256f8c1ec5a5091a974a
Tony Eng <tony.eng@telus.com>
2023-01-23 23:48:28 -0800

    add ex1 create class and puts count

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,27 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# class Store < ActiveRecord::Base
+#   # attr_accessor :location, :annual_revenue, :men_apparel, :women_apparel
+
+#   def initialize(location, annual_revenue, men_apparel, women_apparel)
+#     @location = location
+#     @annual_revenue = annual_revenue
+#     @men_apparel = men_apparel
+#     @women_apparel = women_apparel
+#   end
+# end
+
+# burnaby = Store.create("Burnaby", 300000, true, true)
+# # richmond = Store.new("Richmond", 1260000, false, true)
+# # gastown = Store.new("Gastown", 190000, true, false)
+
+# puts burnaby
+burnaby = Store.create!(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create!(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create!(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+puts burnaby.name
+puts richmond.name
+puts gastown.name
+
+puts Store.count
