714ce37cef831bfd19c3ccdac6d8c1ea35f131a7
Tony Eng <tony.eng@telus.com>
2023-01-24 01:44:28 -0800

    add ex7 created employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Chu", hourly_rate: 23)
+@store1.employees.create(first_name: "Julie", last_name: "Smith", hourly_rate: 56)
+@store2.employees.create(first_name: "Robert", last_name: "An", hourly_rate: 15)
+@store2.employees.create(first_name: "Jordan", last_name: "Wong", hourly_rate: 70)
+@store2.employees.create(first_name: "Scotty", last_name: "Pippen", hourly_rate: 22)
