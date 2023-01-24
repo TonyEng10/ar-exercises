118d9b64802c76b66a8b3f05650853939a09eaaa
Tony Eng <tony.eng@telus.com>
2023-01-24 01:37:45 -0800

    add ex5 calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum_revenue_all_stores = Store.sum("annual_revenue")
+puts "sum_revenue_all_stores #{sum_revenue_all_stores}"
+store_count = Store.count
+avg_store_revenue = sum_revenue_all_stores / store_count
+
+puts "avg_store_revenue #{avg_store_revenue}"
+
+stores_over_milly = Store.where("annual_revenue > 1000000").count
