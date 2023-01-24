6e4010ce9a6f9b3c33ccf5ab3407ce773897b5f7
Tony Eng <tony.eng@telus.com>
2023-01-23 23:56:15 -0800

    add ex2 find_by id change name

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -20,9 +20,9 @@ puts "----------"
 # # gastown = Store.new("Gastown", 190000, true, false)
 
 # puts burnaby
-burnaby = Store.create!(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-richmond = Store.create!(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-gastown = Store.create!(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 puts burnaby.name
 puts richmond.name
 puts gastown.name
