09915a955f33ec9009563461347e9650107cf13b
Tony Eng <tony.eng@telus.com>
2023-01-24 21:00:26 -0800

    add ex7 validations complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -15,5 +15,29 @@ puts "----------"
 # def first_name_present
 #   errors.add(:first_name, "First name is required") if first_name.blank?
 # end
+# puts @store2
+# test_employee = @store2.employees.create( hourly_rate: 10)
+# puts test_employee.hourly_rate
 
-@store2.employees.create(hourly_rate: 50)
+coquitlam = Store.create(name: "coquitlam", annual_revenue: 3000000, mens_apparel: true, womens_apparel: true)
+t_employee = coquitlam.employees.create( hourly_rate: 5)
+puts t_employee.valid?
+puts t_employee.hourly_rate
+
+t2_employee = coquitlam.employees.create(first_name:"Tony", last_name:"hello", hourly_rate: 100)
+puts t2_employee.valid?
+puts t2_employee.first_name
+
+t3_employee = coquitlam.employees.create( hourly_rate: 1)
+puts t3_employee.valid?
+puts t3_employee.hourly_rate
+
+t4_employee = Employee.create(first_name:"ace", last_name:"luffy", hourly_rate: 150)
+puts t4_employee.valid?
+puts t4_employee.hourly_rate
+
+puts "coquitlam store #{coquitlam.valid?}"
+
+calgary = Store.create(name:"calgary")
+
+puts calgary.valid?
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,9 +1,11 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
 
-  validates :first_name, presence: true, on: :create!
-  
-  
+  validates :first_name, presence: true, length: {minimum: 3} 
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_interger: true }, inclusion: { in: 40..200 }
+  validates :store_id, presence: true
+
 end
 
 
